b7f624618fc643de0889b0e324cc7042de9393ca
Tony Xu <tony.xu@userful.com>
2023-05-18 19:17:56 -0600

    Try optuna

---
+++ main/test_super.py
@@ -0,0 +1,112 @@
+# Copyright 2023 LIN Yi. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import os
+import time 
+import argparse
+
+import retro
+from stable_baselines3 import PPO
+
+from street_fighter_super_wrapper import StreetFighterSuperWrapper
+
+DEFAULT_MODEL_FILE = r"trained_models/ppo_ryu_2000000_steps_updated" # Speicify the model file to load. Model "ppo_ryu_2500000_steps_updated" is capable of beating the final stage (Bison) of the game.
+#MODEL_NAME = r"ppo_ryu_10000000_steps"
+# Model notes:
+# ppo_ryu_2000000_steps_updated: Just beginning to overfit state, generalizable but not quite capable.
+# ppo_ryu_2500000_steps_updated: Approaching the final overfitted state, cannot dominate first round but partially generalizable. High chance of beating the final stage.
+# ppo_ryu_3000000_steps_updated: Near the final overfitted state, almost dominate first round but barely generalizable.
+# ppo_ryu_7000000_steps_updated: Overfitted, dominates first round but not generalizable. 
+DEFAULT_STATE = "Champion.Level1.RyuVsGuile"
+
+
+def make_env(game, state, reset_type, rendering, p2ai):
+    def _init():
+        players = 1
+        if p2ai:
+            players = 2
+        env = retro.make(
+            game=game, 
+            state=state, 
+            use_restricted_actions=retro.Actions.FILTERED,
+            obs_type=retro.Observations.IMAGE,
+            players=players
+        )
+        env = StreetFighterSuperWrapper(env, reset_type=reset_type, rendering=rendering, p2ai=p2ai)
+        return env
+    return _init
+
+parser = argparse.ArgumentParser(description='Reset game stats')
+parser.add_argument('--reset', choices=['round', 'match', 'game'], help='Reset stats for a round, a match, or the whole game', default='round')
+parser.add_argument('--model-file', help='The model file to load. By default trained_models/ppo_ryu_2000000_steps_updated', default=DEFAULT_MODEL_FILE)
+parser.add_argument('--state', help='The state file to load. By default Champion.Level1.RyuVsGuile', default=DEFAULT_STATE)
+parser.add_argument('--skip-render', action='store_true', help='Whether to skip to render the game screen.')
+parser.add_argument('--random-action', action='store_true', help='Use ramdom action instead of')
+parser.add_argument('--num-episodes', type=int, help='Play how many episodes', default=30)
+parser.add_argument('--P2AI', action='store_true', help='AI control player 2.')
+
+args = parser.parse_args()
+
+print("command line args:" + str(args))
+
+game = "StreetFighterIISpecialChampionEdition-Genesis"
+#env = make_env(game, state="Champion.Level12.RyuVsBison")()
+env = make_env(game, state=args.state, reset_type=args.reset, rendering=not args.skip_render, p2ai=args.P2AI)()
+# model = PPO("CnnPolicy", env)
+
+if not args.random_action:
+    model = PPO.load(args.model_file, env=env)
+
+obs = env.reset()
+done = False
+
+episode_reward_sum = 0
+num_victory = 0
+
+print("\nFighting Begins!\n")
+
+for _ in range(args.num_episodes):
+    done = False
+    
+    obs = env.reset()
+
+    total_reward = 0
+
+    while not done:
+        timestamp = time.time()
+
+        if args.random_action:
+            action = env.action_space.sample()
+        else:
+            action, _states = model.predict(obs)
+
+        obs, reward, done, info = env.step(action)
+
+        if reward != 0:
+            total_reward += reward
+            print("Reward: {:.3f}, playerHP: {}, enemyHP:{}".format(reward, info['agent_hp'], info['enemy_hp']))
+        
+
+    if info['enemy_hp'] < 0:
+        print("Victory!")
+        num_victory += 1
+
+    print("Total reward: {}\n".format(total_reward))
+    episode_reward_sum += total_reward
+
+
+env.close()
+print("Winning rate: {}".format(1.0 * num_victory / args.num_episodes))
+if args.random_action:
+    print("Average reward for random action: {}".format(episode_reward_sum/args.num_episodes))
+else:
+    print("Average reward for {}: {}".format(args.model_file, episode_reward_sum/args.num_episodes))
---
+++ main/train_super_opt.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import argparse
+
+import retro
+import optuna
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.vec_env import SubprocVecEnv
+
+from street_fighter_super_wrapper import StreetFighterSuperWrapper
+
+LOG_DIR = 'logs'
+OPT_DIR = 'opts'
+os.makedirs(LOG_DIR, exist_ok=True)
+
+DEFAULT_STATE = "Champion.Level1.RyuVsGuile"
+# Linear scheduler
+def linear_schedule(initial_value, final_value=0.0):
+
+    if isinstance(initial_value, str):
+        initial_value = float(initial_value)
+        final_value = float(final_value)
+        assert (initial_value > 0.0)
+
+    def scheduler(progress):
+        return final_value + progress * (initial_value - final_value)
+
+    return scheduler
+
+def make_env(game, state, reset_type, rendering, p2ai = False, seed=0):
+    def _init():
+        players = 1
+        if p2ai:
+            players = 2        
+        env = retro.make(
+            game=game, 
+            state=state, 
+            use_restricted_actions=retro.Actions.FILTERED, 
+            obs_type=retro.Observations.IMAGE,
+            players=players
+        )
+        env = StreetFighterSuperWrapper(env, rendering=rendering, reset_type=reset_type, p2ai=p2ai)
+        env = Monitor(env)
+        env.seed(seed)
+        return env
+    return _init
+
+def optimize_ppo(trail):
+    return {
+        'n_steps': trail.suggest_int('n_steps', 2048, 8192),
+        'batch_size': trail.suggest_int('batch_size', 64, 1024),
+        'n_epochs': trail.suggest_int('batch_size', 3, 8),
+        'gamma': trail.suggest_loguniform('gamma', 0.8, 0.99),
+        'learning_rate': trail.suggest_loguniform('learning_rate', 1e-5, 1e-4),
+        'clip_range': trail.suggest_loguniform('clip_range', 0.1, 0.4),
+        'gae_lambda': trail.suggest_loguniform('gae_lambda', 0.8, 0.99),
+    }
+
+def make_optimize_agent(args):
+    # Run a training loop and return mean reward 
+    def optimize_agent(trial):
+        try:
+            model_params = optimize_ppo(trial) 
+
+            # Create environment 
+            game = "StreetFighterIISpecialChampionEdition-Genesis"
+            env = SubprocVecEnv([make_env(game, state=args.state, reset_type=args.reset, rendering=args.render, seed=i) for i in range(args.num_env)])
+
+            # Create algo 
+            model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=0, **model_params)
+            model.learn(total_timesteps=args.total_steps)
+
+            # Evaluate model 
+            mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=5)
+            env.close()
+
+            SAVE_PATH = os.path.join(OPT_DIR, 'trial_{}_best_model'.format(trial.number))
+            model.save(SAVE_PATH)
+
+            return mean_reward
+
+        except Exception as e:
+            print (e)
+            return -1000
+    return optimize_agent
+    
+def main():
+    parser = argparse.ArgumentParser(description='Reset game stats')
+    parser.add_argument('--reset', choices=['round', 'match', 'game'], help='Reset stats for a round, a match, or the whole game', default='round')
+    parser.add_argument('--state', help='The state file to load. By default Champion.Level1.RyuVsGuile', default=DEFAULT_STATE)
+    parser.add_argument('--render', action='store_true', help='Whether to render the game screen.')
+    parser.add_argument('--num-env', type=int, help='How many envirorments to create', default=16)
+    parser.add_argument('--total-steps', type=int, help='How many total steps to train', default=100000)
+    parser.add_argument('--opt-trials', type=int, help='How many optimization trials', default=100)
+
+    args = parser.parse_args()
+
+    print("command line args:" + str(args))
+                                 
+    optimize_agent = make_optimize_agent(args)
+    # Creating the experiment 
+    study = optuna.create_study(direction='maximize')
+    study.optimize(optimize_agent, n_trials=args.opt_trials, n_jobs=1)
+
+if __name__ == "__main__":
+    main()
